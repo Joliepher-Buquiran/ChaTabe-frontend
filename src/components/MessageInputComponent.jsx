3133faf117a50216ddf52ff9bc7710b540d71699
JapP <buquiranjapjap@gmail.com>
2025-12-04 09:37:24 +0800

    final loading

--- src/components/MessageInputComponent.jsx
+++ src/components/MessageInputComponent.jsx
@@ -199,7 +199,7 @@ export const MessageInputComponent = ({senderId, receiverId,senderUsername,recei
         <div className="flex flex-col items-center py-4 bg-red-700 px-5 gap-2 rounded-lg">
 
 
-            {loadingLoading && (
+            {sendLoading && (
                 <div className="w-full flex justify-center py-2">
                     <div className="flex space-x-2">
                         <div className="w-2 h-2 bg-purple-500 rounded-full animate-bounce"></div>
